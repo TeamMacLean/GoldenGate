0b972b957c84aebf1947a60ccea0ecdabce8e252
Martin Page <martin.page@tsl.ac.uk>
2014-06-10 17:20:02 +0100

    folder reader test

--- app.pl
+++ app.pl
@@ -1,4 +1,21 @@
+use strict;
+use warnings;
+
 use Mojolicious::Lite;
+
+my $appRoot = './';
+
+#TODO TEST
+my $dir = $appRoot.'data';
+    opendir(DIR, $dir) or die $!;
+    while (my $file = readdir(DIR)) {
+        #ignore files beginning with a period
+        next if ($file =~ m/^\./);
+	print "$file\n";
+    }
+closedir(DIR);
+exit 0;
+
 
 get '/' => 'index';
 app->start;
