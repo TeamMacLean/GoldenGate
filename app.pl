f4cd4dcb5c81cb8f98a1798ba9aea74a3cb13ed5
Martin Page <martin.page@tsl.ac.uk>
2014-06-10 17:25:43 +0100

    disable test; demo select options

--- app.pl
+++ app.pl
@@ -1,20 +1,19 @@
 use strict;
 use warnings;
-
 use Mojolicious::Lite;
 
 my $appRoot = './';
 
 #TODO TEST
-my $dir = $appRoot.'data';
-    opendir(DIR, $dir) or die $!;
-    while (my $file = readdir(DIR)) {
-        #ignore files beginning with a period
-        next if ($file =~ m/^\./);
-	print "$file\n";
-    }
-closedir(DIR);
-exit 0;
+#my $dir = $appRoot.'data';
+#    opendir(DIR, $dir) or die $!;
+#    while (my $file = readdir(DIR)) {
+#        ignore files beginning with a period
+#        next if ($file =~ m/^\./);
+#	print "$file\n";
+#    }
+#closedir(DIR);
+#exit 0;
 
 
 get '/' => 'index';
